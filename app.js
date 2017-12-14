86a729e5bd0a59634f47b1c5b7dcf44ee0ff9040
urban-knight <fandul.ant@gmail.com>
2017-12-14 20:12:10 +0200

    Add i18n translation

--- app.js
+++ app.js
@@ -1,7 +1,10 @@
 const dotenv = require('dotenv').config({ path: './config/.env' });
+const path = require("path");
 const express = require('express');
+const i18n = require("i18n-express");
 const bodyParser = require('body-parser');
 const { error } = require("./middleware");
+const cookieParser = require('cookie-parser');
 const methodOverride = require('method-override');
 
 const { promisifyAll } = require('bluebird');
@@ -14,6 +17,13 @@ app.use(bodyParser.urlencoded({ extended: true }));
 app.set("view engine", "ejs");
 app.use('/static', express.static(__dirname + "/public"));
 app.use(methodOverride("_method"));
+
+// --- i18n Config --- //
+app.use(i18n({
+    translationsPath: path.join(__dirname, 'lang'),
+    siteLangs: ["en", "ru", "ua"],
+    textsVarName: 'translation'
+}));
 
 // --- APP ROUTINGS --- //
 app.use("/", indexRouter);
