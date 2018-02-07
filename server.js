6861ed3dbac05ea7d4f77ffcf3f813d96d6152f0
urban-knight <fandul.ant@gmail.com>
2018-02-08 01:35:15 +0200

    Add Hreflang links to head

--- server.js
+++ server.js
@@ -6,22 +6,6 @@ const https = require('https');
 let http_port = process.env.APP_HTTP_PORT || 80;
 let https_port = process.env.APP_HTTPS_PORT || 443;
 let host = process.env.APP_HOST || "localhost";
-// let key =  fs.readFileSync('./keys/private.key');
-// let cert = fs.readFileSync('./keys/certificate.pem');
-
-// // Route all Traffic to Secure Server
-// app.all('*', function (req, res, next) {
-//   if (req.secure) {
-//     return next();
-//   };
-//   res.redirect('https://hive-studio.net:' + https_port + req.url);
-//   // res.redirect('https://'+req.hostname+':'+HTTPS_PORT+req.url);
-// });
-
-// // HTTPS
-// var secureServer = https.createServer({key: key, cert: cert}, app).listen(https_port, () => {
-//     console.log('Secure Website Server listening on port ' + https_port);
-// });
 
 var insecureServer = http.createServer(app).listen(http_port, () => {
   console.log('Insecure Website Server listening on port ' + http_port);
